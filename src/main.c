7ec0f66e3cad3de4ccf3b30b5967e2beab5d26ee
therippa <therippa@gmail.com>
2023-07-25 22:39:27 -0700

    add debug file

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// make sure to change the project and executable name in CMakeLists.txt
+// make sure to change the project and executable name in CMakeLists.txt, and git upstream
 
 int main() {
     printf("Hello, World!\n");
